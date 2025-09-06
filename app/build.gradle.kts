b1847f0edb278729ef8ea1a44d8b6c51aaf9e555
Ronal9y <142257709+Ronal9y@users.noreply.github.com>
2025-09-06 17:50:51 -0400

    se agregaron las dependencias necesarias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,4 +71,22 @@ dependencies {
     implementation("com.google.dagger:hilt-android:2.57.1")
     ksp("com.google.dagger:hilt-android-compiler:2.57.1")
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.3")
+    implementation("androidx.compose.runtime:runtime-livedata:1.9.0")
+    implementation("androidx.compose.runtime:runtime:1.9.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.9.3")
+
+   //navegacion
+    implementation("androidx.navigation:navigation-compose:2.8.0-rc01")
+
+// Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+
+// ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.3")
+
+    // Iconos de Material
+    implementation("androidx.compose.material:material-icons-extended:1.7.8")
 }
