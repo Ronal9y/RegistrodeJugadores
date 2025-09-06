7ecc7deac4b09650057268e3a1cabb8f1eab9e92
Ronal9y <142257709+Ronal9y@users.noreply.github.com>
2025-09-06 18:01:03 -0400

    se creo una clase sellada para la navegacion

---
+++ app/src/main/java/edu/ucne/registrodejugadores/ui/theme/util/UiEvent.kt
@@ -0,0 +1,7 @@
+package edu.ucne.registrodejugadores.ui.theme.util
+
+sealed class UiEvent {
+    data class ShowMessage(val message: String) : UiEvent()
+    object NavigateBack : UiEvent()
+    data class NavigateTo(val route: String) : UiEvent()
+}
