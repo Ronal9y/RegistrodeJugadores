28925bd20f800eef78658fe1b238de1d54eedce9
Ronal9y <142257709+Ronal9y@users.noreply.github.com>
2025-09-06 18:30:40 -0400

    configura y proporciona todas las dependencias que la aplicación necesita mediante inyección de dependencias

---
+++ app/src/main/java/edu/ucne/registrodejugadores/data/di/AppModule.kt
@@ -0,0 +1,39 @@
+package edu.ucne.registrodejugadores.data.di
+
+import android.content.Context
+import androidx.room.Room
+import edu.ucne.registrodejugadores.data.local.Jugadores
+import edu.ucne.registrodejugadores.data.repository.JugadorRepositoryImpl
+import edu.ucne.registrodejugadores.domain.repository.JugadorRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import edu.ucne.registrodejugadores.data.local.dao.JugadorDao
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideJugadorDatabase(@ApplicationContext context: Context): Jugadores {
+        return Room.databaseBuilder(
+            context,
+            Jugadores::class.java,
+            "jugadoresDb"
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideJugadorDao(database: Jugadores) = database.jugadorDao()
+
+    @Provides
+    @Singleton
+    fun provideJugadorRepository(dao: JugadorDao): JugadorRepository {
+        return JugadorRepositoryImpl(dao)
+    }
+}
