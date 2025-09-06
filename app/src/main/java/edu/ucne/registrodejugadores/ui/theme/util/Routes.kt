a1dec9963c4a94125d971f0bef7adf9b2a5830b7
Ronal9y <142257709+Ronal9y@users.noreply.github.com>
2025-09-06 17:49:42 -0400

    se crearon las rutas para la navegacion

---
+++ app/src/main/java/edu/ucne/registrodejugadores/ui/theme/util/Routes.kt
@@ -0,0 +1,8 @@
+package edu.ucne.registrodejugadores.ui.theme.util
+
+object Routes {
+    const val JUGADOR_LIST = "jugador_list"
+    const val JUGADOR_FORM = "jugador_form"
+    const val SELECCION_JUGADORES = "seleccion_jugadores"
+    const val GAME_SCREEN = "game_screen"
+}
