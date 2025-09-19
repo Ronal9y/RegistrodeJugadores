5048dcdc7236b041edeef2b56549a06dc758ed79
Ronal9y <142257709+Ronal9y@users.noreply.github.com>
2025-09-19 11:05:12 -0400

    se modificó la función insert en PartidaDao para retornar un Long y se agregó una nueva consulta getById para obtener una partida por su ID

--- app/src/main/java/edu/ucne/registrodejugadores/data/local/dao/PartidaDao.kt
+++ app/src/main/java/edu/ucne/registrodejugadores/data/local/dao/PartidaDao.kt
@@ -14,11 +14,14 @@ interface PartidaDao {
     suspend fun getPartidaById(id: Int): PartidaEntity?
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insert(entity: PartidaEntity)
+    suspend fun insert(entity: PartidaEntity): Long
 
     @Update
     suspend fun update(entity: PartidaEntity)
 
     @Delete
     suspend fun delete(entity: PartidaEntity)
+
+    @Query("SELECT * FROM Partida WHERE partidaId = :id")
+    suspend fun getById(id: Int): PartidaEntity
 }
