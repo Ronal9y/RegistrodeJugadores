d9809a9095e40092a00987e36be79b39e2fc01b9
Ronal9y <142257709+Ronal9y@users.noreply.github.com>
2025-09-19 09:50:27 -0400

    se le agrego 2 funciones que serviran para guardar el tablero en la base de datos para poder reanudar la partida

--- app/src/main/java/edu/ucne/registrodejugadores/data/local/entity/PartidaEntity.kt
+++ app/src/main/java/edu/ucne/registrodejugadores/data/local/entity/PartidaEntity.kt
@@ -3,13 +3,22 @@ package edu.ucne.registrodejugadores.data.local.entity
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import edu.ucne.registrodejugadores.domain.model.Partida
+import edu.ucne.registrodejugadores.ui.theme.screen.Juego.Player
 
 @Entity(tableName = "Partida")
 data class PartidaEntity(
-    @PrimaryKey(autoGenerate = true) val partidaId: Int = 0,
+    @PrimaryKey(autoGenerate = true)
+    val partidaId: Int = 0,
     val fecha: String,
     val jugador1Id: Int,
     val jugador2Id: Int,
     val ganadorId: Int,
-    val esFinalizada: Boolean
+    val esFinalizada: Boolean,
+    val board: String
 )
+
+fun Array<Player?>.toCsv(): String =
+    joinToString(",") { it?.name ?: "null" }
+
+fun String.toPlayerArray(): Array<Player?> =
+    split(",").map { if (it == "null") null else Player.valueOf(it) }.toTypedArray()
