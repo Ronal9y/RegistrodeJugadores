db05b475321442aab5516c7d607c78de30dee7d4
Ronal9y <142257709+Ronal9y@users.noreply.github.com>
2025-09-19 11:07:40 -0400

    modificado el método insertPartida en PartidaRepository para retornar un Long

--- app/src/main/java/edu/ucne/registrodejugadores/domain/repository/PartidaRepository.kt
+++ app/src/main/java/edu/ucne/registrodejugadores/domain/repository/PartidaRepository.kt
@@ -6,7 +6,8 @@ import kotlinx.coroutines.flow.Flow
 interface PartidaRepository {
     fun getAllPartidas(): Flow<List<Partida>>
     suspend fun getPartidaById(id: Int): Partida?
-    suspend fun insertPartida(partida: Partida)
+    suspend fun insertPartida(partida: Partida): Long
     suspend fun updatePartida(partida: Partida)
     suspend fun deletePartida(partida: Partida)
+
 }
