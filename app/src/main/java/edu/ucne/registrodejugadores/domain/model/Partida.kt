f5a83ff0ae6e6573dc408b6d40a2b992edd8bb60
Ronal9y <142257709+Ronal9y@users.noreply.github.com>
2025-09-19 11:04:25 -0400

    se agregó un nuevo atributo 'board' a la clase Partida y se mejoró la función getFechaActual para utilizar SimpleDateFormat

--- app/src/main/java/edu/ucne/registrodejugadores/domain/model/Partida.kt
+++ app/src/main/java/edu/ucne/registrodejugadores/domain/model/Partida.kt
@@ -11,15 +11,13 @@ data class Partida(
     val jugador1Id: Int = 0,
     val jugador2Id: Int = 0,
     val ganadorId: Int = 0,
-    val esFinalizada: Boolean
+    val esFinalizada: Boolean,
+    val board: String = "null,null,null,null,null,null,null,null,null"
 ) : Parcelable {
     companion object {
         fun getFechaActual(): String {
-            val calendar = Calendar.getInstance()
-            val dia = calendar.get(Calendar.DAY_OF_MONTH)
-            val mes = calendar.get(Calendar.MONTH) + 1
-            val anio = calendar.get(Calendar.YEAR)
-            return "$dia/$mes/$anio"
+            val sdf = java.text.SimpleDateFormat("dd/MM/yyyy HH:mm", java.util.Locale.getDefault())
+            return sdf.format(java.util.Date())
         }
     }
 }
