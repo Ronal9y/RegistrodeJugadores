b35681da705e285c5329552ceeb2baf4586a2342
Ronal9y <142257709+Ronal9y@users.noreply.github.com>
2025-09-20 20:30:47 -0400

    agregar entidad PartidaEntity con propiedades y funciones para manejo de jugadores

---
+++ app/src/main/java/edu/ucne/registrodejugadores/data/local/entity/PartidaEntity.kt
@@ -0,0 +1,24 @@
+package edu.ucne.registrodejugadores.data.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import edu.ucne.registrodejugadores.domain.model.Partida
+import edu.ucne.registrodejugadores.presentation.Juego.Player
+
+@Entity(tableName = "Partida")
+data class PartidaEntity(
+    @PrimaryKey(autoGenerate = true)
+    val partidaId: Int = 0,
+    val fecha: String,
+    val jugador1Id: Int,
+    val jugador2Id: Int,
+    val ganadorId: Int,
+    val esFinalizada: Boolean,
+    val board: String
+)
+
+fun Array<Player?>.toCsv(): String =
+    joinToString(",") { it?.name ?: "null" }
+
+fun String.toPlayerArray(): Array<Player?> =
+    split(",").map { if (it == "null") null else Player.valueOf(it) }.toTypedArray()
