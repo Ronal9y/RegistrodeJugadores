5ec63d17cc69988503492eb66f4af44a9c50a9a9
Ronal9y <142257709+Ronal9y@users.noreply.github.com>
2025-09-20 20:32:25 -0400

    agregar interfaz PartidaRepository para operaciones de gestión de partidas

---
+++ app/src/main/java/edu/ucne/registrodejugadores/domain/repository/PartidaRepository.kt
@@ -0,0 +1,13 @@
+package edu.ucne.registrodejugadores.domain.repository
+
+import edu.ucne.registrodejugadores.domain.model.Partida
+import kotlinx.coroutines.flow.Flow
+
+interface PartidaRepository {
+    fun getAllPartidas(): Flow<List<Partida>>
+    suspend fun getPartidaById(id: Int): Partida?
+    suspend fun insertPartida(partida: Partida): Long
+    suspend fun updatePartida(partida: Partida)
+    suspend fun deletePartida(partida: Partida)
+
+}
