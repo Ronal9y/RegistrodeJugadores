db8351c97970f2c27b6dfe24fe8e01207f3d811b
Ronal9y <142257709+Ronal9y@users.noreply.github.com>
2025-09-20 20:32:49 -0400

    agregar implementación de PartidaRepository para gestionar operaciones de partidas

---
+++ app/src/main/java/edu/ucne/registrodejugadores/data/repository/PartidaRepositoryImpl.kt
@@ -0,0 +1,38 @@
+package edu.ucne.registrodejugadores.data.repository
+
+import edu.ucne.registrodejugadores.data.local.dao.PartidaDao
+import edu.ucne.registrodejugadores.data.local.entity.PartidaEntity
+import edu.ucne.registrodejugadores.domain.model.Partida
+import edu.ucne.registrodejugadores.domain.repository.PartidaRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class PartidaRepositoryImpl @Inject constructor(
+    private val dao: PartidaDao
+) : PartidaRepository {
+
+    override fun getAllPartidas(): Flow<List<Partida>> =
+        dao.getAllPartidas().map { list -> list.map { it.toDomain() } }
+
+    override suspend fun getPartidaById(id: Int): Partida? =
+        dao.getPartidaById(id)?.toDomain()
+
+    override suspend fun insertPartida(partida: Partida): Long {
+        return dao.insert(partida.toEntity())
+    }
+
+    override suspend fun updatePartida(partida: Partida) =
+        dao.update(partida.toEntity())
+
+    override suspend fun deletePartida(partida: Partida) =
+        dao.delete(partida.toEntity())
+
+    private fun PartidaEntity.toDomain() = Partida(
+        partidaId, fecha, jugador1Id, jugador2Id, ganadorId, esFinalizada, board
+    )
+
+    private fun Partida.toEntity() = PartidaEntity(
+        partidaId, fecha, jugador1Id, jugador2Id, ganadorId, esFinalizada,board
+    )
+}
