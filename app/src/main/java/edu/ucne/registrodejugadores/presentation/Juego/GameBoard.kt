e79cf53f5b3d53ed5a4ef49aba88d19f8a583382
Ronal9y <142257709+Ronal9y@users.noreply.github.com>
2025-09-20 20:25:48 -0400

    renombrar paquete de GameBoard a presentación

--- app/src/main/java/edu/ucne/registrodejugadores/presentation/Juego/GameBoard.kt
+++ app/src/main/java/edu/ucne/registrodejugadores/presentation/Juego/GameBoard.kt
@@ -1,4 +1,4 @@
-package edu.ucne.registrodejugadores.ui.theme.screen.Juego
+package edu.ucne.registrodejugadores.presentation.Juego
 
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.clickable
@@ -15,7 +15,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
-import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.unit.dp
 
 @Composable
