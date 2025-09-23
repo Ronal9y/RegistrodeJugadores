7e6f0299cbd35d5bd002d67690106948f6200344
Ronal9y <142257709+Ronal9y@users.noreply.github.com>
2025-09-23 16:30:29 -0400

    solo se le removio lo que se le habia agregado porlo que no se le hizo nada

--- app/src/main/java/edu/ucne/registrodejugadores/data/mapper/JugadorMapper.kt
+++ app/src/main/java/edu/ucne/registrodejugadores/data/mapper/JugadorMapper.kt
@@ -1,7 +1,9 @@
 package edu.ucne.registrodejugadores.data.mapper
 
 import edu.ucne.registrodejugadores.data.local.entity.JugadorEntity
+import edu.ucne.registrodejugadores.data.local.entity.LogroEntity
 import edu.ucne.registrodejugadores.domain.model.Jugador
+import edu.ucne.registrodejugadores.domain.model.Logro
 
 fun JugadorEntity.toJugador(): Jugador = Jugador(
     id = jugadorId,
@@ -13,4 +15,16 @@ fun Jugador.toEntity(): JugadorEntity = JugadorEntity(
     jugadorId = id ?: 0,
     nombres = nombres,
     partidas = partidas
+)
+
+fun LogroEntity.asExternalModel(): Logro = Logro(
+    logroId = logroId,
+   titulo = titulo,
+    descripcion = descripcion
+)
+
+fun Logro.toEntity(): LogroEntity = LogroEntity(
+    logroId = logroId ?: 0,
+    titulo = titulo,
+    descripcion = descripcion
 )
