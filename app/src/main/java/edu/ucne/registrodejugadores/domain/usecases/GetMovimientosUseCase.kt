b1af9ab93534f539447d2cf6e7aee51fd73da599
Ronal9y <142257709+Ronal9y@users.noreply.github.com>
2025-10-26 17:41:18 -0400

    Se creo caso de uso

--- app/src/main/java/edu/ucne/registrodejugadores/domain/usecases/GetMovimientosUseCase.kt
+++ app/src/main/java/edu/ucne/registrodejugadores/domain/usecases/GetMovimientosUseCase.kt
@@ -1,11 +1,18 @@
 package edu.ucne.registrodejugadores.domain.usecases
 
+import edu.ucne.registrodejugadores.data.remote.Resource
+import edu.ucne.registrodejugadores.domain.model.Movimiento
 import edu.ucne.registrodejugadores.domain.repository.MovimientosRepository
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class GetMovimientosUseCase @Inject constructor(
     private val repository: MovimientosRepository
 ) {
-    operator fun invoke(partidaId: Int) =
+    operator fun invoke(partidaId: Int): Flow<Resource<List<Movimiento>>> =
         repository.getMovimientos(partidaId)
+
+
+    suspend fun cargarMovimientosDesdeAPI(partidaId: Int): Resource<List<Movimiento>> =
+        repository.cargarMovimientosDesdeAPI(partidaId)
 }
