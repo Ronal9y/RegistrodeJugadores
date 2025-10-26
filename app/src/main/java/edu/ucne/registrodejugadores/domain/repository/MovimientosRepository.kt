b02e0c1499165899920c0a9200d7ce4162208ac9
Ronal9y <142257709+Ronal9y@users.noreply.github.com>
2025-10-26 17:38:48 -0400

    Se actualicemos la interfaz del Repository

--- app/src/main/java/edu/ucne/registrodejugadores/domain/repository/MovimientosRepository.kt
+++ app/src/main/java/edu/ucne/registrodejugadores/domain/repository/MovimientosRepository.kt
@@ -6,5 +6,9 @@ import kotlinx.coroutines.flow.Flow
 
 interface MovimientosRepository {
     fun getMovimientos(partidaId: Int): Flow<Resource<List<Movimiento>>>
+    suspend fun createMovimientoLocal(movimiento: Movimiento): Resource<Movimiento>
     suspend fun postMovimiento(movimiento: Movimiento): Resource<Unit>
+    suspend fun deleteMovimiento(id: String): Resource<Unit>
+    suspend fun postPendingMovimientos(): Resource<Unit>
+    suspend fun cargarMovimientosDesdeAPI(partidaId: Int): Resource<List<Movimiento>>
 }
