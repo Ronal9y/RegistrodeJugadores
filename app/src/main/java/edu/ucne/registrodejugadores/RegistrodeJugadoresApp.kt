c44d6ebdbf9de7eeb10f24e551238efe69cc3a9f
Ronal9y <142257709+Ronal9y@users.noreply.github.com>
2025-10-26 17:53:39 -0400

    Inicialización en Application

--- app/src/main/java/edu/ucne/registrodejugadores/RegistrodeJugadoresApp.kt
+++ app/src/main/java/edu/ucne/registrodejugadores/RegistrodeJugadoresApp.kt
@@ -1,7 +1,19 @@
 package edu.ucne.registrodejugadores
 
 import android.app.Application
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
+import edu.ucne.registrodejugadores.worker.AppWorkerFactory
+import javax.inject.Inject
 
 @HiltAndroidApp
-class RegistrodeJugadoresApp : Application()
+class RegistrodeJugadoresApp : Application(), Configuration.Provider {
+
+    @Inject
+    lateinit var workerFactory: AppWorkerFactory
+
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
+}
