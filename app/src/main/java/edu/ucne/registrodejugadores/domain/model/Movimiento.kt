c66920c5620809efe2f342b855eb5fc62b4ed53b
Ronal9y <142257709+Ronal9y@users.noreply.github.com>
2025-10-26 17:50:20 -0400

    se  modifico el id

--- app/src/main/java/edu/ucne/registrodejugadores/domain/model/Movimiento.kt
+++ app/src/main/java/edu/ucne/registrodejugadores/domain/model/Movimiento.kt
@@ -1,6 +1,9 @@
 package edu.ucne.registrodejugadores.domain.model
 
+import java.util.UUID
+
 data class Movimiento(
+    val id: String = UUID.randomUUID().toString(),
     val partidaId: Int?,
     val jugador: String,
     val posicionFila: Int,
