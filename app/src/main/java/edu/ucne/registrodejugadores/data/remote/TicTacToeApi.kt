d2d643dbab5e9d5fd138a65049dcb811b3eccbd7
Ronal9y <142257709+Ronal9y@users.noreply.github.com>
2025-10-26 17:58:20 -0400

    se actualizo la interfaz de la API

--- app/src/main/java/edu/ucne/registrodejugadores/data/remote/TicTacToeApi.kt
+++ app/src/main/java/edu/ucne/registrodejugadores/data/remote/TicTacToeApi.kt
@@ -1,15 +1,30 @@
 package edu.ucne.registrodejugadores.data.remote
 
 import edu.ucne.registrodejugadores.data.remote.dto.movimientosDto
+import edu.ucne.registrodejugadores.data.remote.dto.PartidaDto
+import edu.ucne.registrodejugadores.data.remote.dto.PostMovimientoDto
+import edu.ucne.registrodejugadores.data.remote.dto.PostPartidaDto
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface TicTacToeApi {
+
+    // MOVIMIENTOS
     @GET("api/Movimientos/{partidaId}")
-    suspend fun getMovimientos(@Path("partidaId") id: Int): List<movimientosDto>
+    suspend fun getMovimientos(@Path("partidaId") partidaId: Int): List<movimientosDto>
 
     @POST("api/Movimientos")
-    suspend fun postMovimiento(@Body movimiento: movimientosDto)
+    suspend fun postMovimiento(@Body movimiento: PostMovimientoDto)
+
+    // PARTIDAS
+    @GET("api/Partidas/{partidaId}")
+    suspend fun getPartida(@Path("partidaId") partidaId: Int): PartidaDto
+
+    @GET("api/Partidas")
+    suspend fun getPartidas(): List<PartidaDto>
+
+    @POST("api/Partidas")
+    suspend fun postPartida(@Body partida: PostPartidaDto): PartidaDto
 }
